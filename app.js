f620bf74615a26ffe662f8755d0ae2b8d0c99932
noor-khan <foruconnect@gmail.com>
2020-07-20 14:46:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const reload = require("reload");
+// const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+
+reload(app).then(function (reloadReturned){
+  app.listen(3000, () => {
+    console.log("Newsletter app is running at port 3000");
+  });
+
+});
+
+
